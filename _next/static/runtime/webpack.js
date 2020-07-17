c7853795594f4009713c616b207a73d1d46e1926
Pedro Álvarez Fernández <palvarez@tuenti.com>
2020-07-17 10:52:43 +0200

    revert to last version

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b7d137df3418639f0f1b";
+/******/ 	var hotCurrentHash = "919ea6103d610fc96b10";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
